b3077c40d2dacc97ec0c92a3423cfd3810e2fb73
GoGiants1 <chooi221@snu.ac.kr>
2024-01-09 18:22:37 +0900

    Fix api code

--- k8s-web-hello/main.py
+++ k8s-web-hello/main.py
@@ -1,6 +1,5 @@
 import os
 from fastapi import FastAPI
-import httpx
 
 app = FastAPI()
 hostname = os.environ.get("HOSTNAME", "unknown")
@@ -9,11 +8,3 @@ hostname = os.environ.get("HOSTNAME", "unknown")
 @app.get("/")
 def read_root():
     return {"hostname": hostname, "version": "v2.0.0"}
-
-
-@app.get("/nginx")
-async def nginx():
-    url = "http://nginx"
-    async with httpx.AsyncClient() as client:
-        r = await client.get(url)
-        return r.json()
--- k8s-web-to-nginx/main.py
+++ k8s-web-to-nginx/main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 hostname = os.environ.get("HOSTNAME", "unknown")
@@ -8,3 +9,11 @@ hostname = os.environ.get("HOSTNAME", "unknown")
 @app.get("/")
 def read_root():
     return {"hostname": hostname, "version": "v2.0.0"}
+
+
+@app.get("/nginx")
+async def nginx():
+    url = "http://nginx"
+    async with httpx.AsyncClient() as client:
+        r = await client.get(url)
+        return r.json()
